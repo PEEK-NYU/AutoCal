7ce6b37384ece6845cb241a04f8e45f46abcf902
Kora <korastuff@gmail.com>
2021-11-04 12:00:10 -0400

    Updating Database Info

--- db/db.py
+++ db/db.py
@@ -4,9 +4,20 @@ At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
 
+import json
+import os
 
-def fetch_pets():
+DEMO_HOME = os.environ["DEMO_HOME"]
+
+EVENTS_DB = f"{DEMO_HOME}/db/events.json"
+# Note: we save events to preserve the possibility of multiple users containing the same event
+
+def get_rooms():
     """
-    A function to return all pets in the data store.
+    A function to return all events in database.
     """
-    return {"tigers": 2, "lions": 3, "zebras": 1}
+    try:
+        with open(EVENTS_DB) as file:
+            return json.loads(file.read())
+    except FileNotFoundError:
+        return None
