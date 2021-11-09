76500ad351b91ab03dbc18945abbd1c8f5772075
Kora <korastuff@gmail.com>
2021-11-09 11:45:17 -0500

    Adding More Database Functions

--- db/db.py
+++ db/db.py
@@ -4,17 +4,88 @@ At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
 
+import json
+import interactions
+
+OK = 0
+NOT_FOUND = 1
+DUPLICATE = 2
+
+EVENTS_DB = o.environ["EVENTS_DB"]
+DB_DIR = f"{EVENTS_DB}/db"
+
+shell_event = {"name":"Software Engineering: CS-UY 4513-C", "descr":"", "start_time":0, "end_time":0, "duration": 90, "location": "2 Metrotech", "unscheduled": False, "attendees":[]}
+
+def write_events(events):
+    with open(EVENTS_DB, 'w') as f:
+        json.dump(rooms, f, indent=4)
+
+
+def get_events():
+    """
+    A function to return all chat rooms.
+    """
+    try:
+        with open(EVENTS_DB) as file:
+            return json.loads(file.read())
+    except FileNotFoundError:
+        print("Rooms db not found.")
+        return None
+
+
+def num_events():
+    """
+    A function to return the number of events in the db.
+    Secondary usage to help with ID generation
+    """
+    events = get_events()
+    return len(events)
+
+
+def add_event():
+    """
+    Add an infoless, unique event to the room database.
+    Until we are using a real DB, we have a potential
+    race condition here.
+    """
+    if events is None:
+        return NOT_FOUND
+    else:
+        events[num_events()+1] = shell_event
+        
+        
+def add_event_by_name(eventname):
+    """
+    Add an event to the room database by name.
+    Until we are using a real DB, we have a potential
+    race condition here.
+    """
+    events = get_events()
+    
+    if events is None:
+        return NOT_FOUND
+    elif eventname in events:
+        return DUPLICATE
+    else:
+        new_event = shell_event
+        new_event["name"] = eventname
+        events[num_events()+1] = new_event
+        write_rooms(rooms)
+        return OK
+
 
 def get_event(event_ID):
     """
     A function to return event in the data store.
     """
-    return {
-        "event_ID": 1234,
-        "name": "Software Engineering",
-        "start_time": 1636038000,
-        "end_time": 1636043400,
-        "duration": 90,
-        "location": "Bern Dibner",
-        "unscheduled": False
-    }
+    events = get_events()
+    return events[event_ID]
+    
+    
+def get_events_by_name(event_name):
+    events = get_events()
+    temp_events = []
+    for key, value in events:
+        if value["name"] == event_name:
+            temp_events.append(key)
+    return temp_events
