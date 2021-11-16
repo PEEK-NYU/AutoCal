4735bba2b42cb8f5f5db7038abd656d5ce63651d
Kora <korastuff@gmail.com>
2021-11-16 14:53:18 -0500

    Updating Endpoints and Tests

--- API/endpoints.py
+++ API/endpoints.py
@@ -6,17 +6,34 @@ The endpoint called `endpoints` will return all available endpoints.
 from http import HTTPStatus
 from flask import Flask
 from flask_restx import Resource, Api
+import werkzeug.exceptions as wz
+
 import db.db as db
 
 app = Flask(__name__)
 api = Api(app)
 
-HELLO = 'Hello'
-WORLD = 'World'
+WORKING_MSG = "I'm working!"
+WORKING_VAL = "\\(^-^)/"
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
+@api.route('/endpoints')
+class Endpoints(Resource):
+    """
+    This class will serve as live, fetchable documentation of what endpoints
+    are available in the system.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    def get(self):
+        """
+        The `get()` method will return a list of available endpoints.
+        """
+        endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
+        return {"Available endpoints": endpoints}
+
+
+@api.route('/test')
+class AppTest(Resource):
     """
     The purpose of the HelloWorld class is to have a simple test to see if the
     app is working at all.
@@ -26,37 +43,79 @@ class HelloWorld(Resource):
         A trivial endpoint to see if the server is running.
         It just answers with "hello world."
         """
-        return {HELLO: WORLD}
+        return {WORKING_MSG: WORKING_VAL}
 
 
-@api.route('/endpoints')
-class Endpoints(Resource):
+@api.route('/list_events')
+class ListEvents(Resource):
     """
-    This class will serve as live, fetchable documentation of what endpoints
-    are available in the system.
-    """
-    def get(self):
-        """
-        The `get()` method will return a list of available endpoints.
-        """
-        endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
-        return {"Available endpoints": endpoints}
-
-
-@api.route('/get_event/<event_id>')
-class GetEvent(Resource):
-    """
-    This class supports fetching an event and
-    adding a user to an event
+    This endpoint returns a list of all events.
     """
     @api.response(HTTPStatus.OK, 'Success')
-    def get(self, event_id):
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def get(self):
         """
-        This method returns info for an event.
+        Returns a list of all chat events.
         """
-        return db.get_event(event_id)
-#    def add_event(self, eventname):
-#        """
-#        This method adds a user to an event
-#        """
-#        return db.add_event_by_name(eventname)
+        events = db.get_events()
+        if events is None:
+            raise (wz.NotFound("Chat event db not found."))
+        else:
+            return events
+
+
+@api.route('/create_event/<eventname>')
+class CreateEvent(Resource):
+    """
+    This class supports adding a chat event.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    @api.response(HTTPStatus.NOT_ACCEPTABLE, 'A duplicate key')
+    def create(self, eventname):
+        """
+        This method adds a event to the event db.
+        """
+        ret = db.add_event(eventname)
+        if ret == db.NOT_FOUND:
+            raise (wz.NotFound("Chat event db not found."))
+        elif ret == db.DUPLICATE:
+            raise (wz.NotAcceptable("Chat event name already exists."))
+
+
+@api.route('/list_users')
+class ListUsers(Resource):
+    """
+    This endpoint returns a list of all users.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def get(self):
+        """
+        Returns a list of all users.
+        """
+        users = db.get_users()
+        if users is None:
+            raise (wz.NotFound("User db not found."))
+        else:
+            return users
+
+
+@api.route('/create_user/<username>')
+class CreateUser(Resource):
+    """
+    This class supports adding a user to the chat event.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    @api.response(HTTPStatus.NOT_ACCEPTABLE, 'A duplicate key')
+    def create(self, username):
+        """
+        This method adds a user to the user databse.
+        """
+        ret = db.add_user(username)
+        if ret == db.NOT_FOUND:
+            raise (wz.NotFound("User db not found."))
+        elif ret == db.DUPLICATE:
+            raise (wz.NotAcceptable("User name already exists."))
+        return f"{username} added."
--- API/tests/test_endpoints.py
+++ API/tests/test_endpoints.py
@@ -2,16 +2,75 @@ from unittest import TestCase, skip
 from flask_restx import Resource, Api
 import random
 
-import API.endpoints as endpoints
-import db.data as db
+import API.endpoints as ep
+import db.db as db
 
+HUGE_NUM = 10000000000000  # any big number will do!
+
+
+def new_entity_name(entity_type):
+    int_name = random.randint(0, HUGE_NUM)
+    return f"new {entity_type}" + str(int_name)
 
 
 class EndpointTestCase(TestCase):
     def setUp(self):
         pass
+
     def tearDown(self):
         pass
-    def test_hello(self):
-        self.assertTrue(False)
-#        self.assertIn(ep.HELLO, ret)
+
+    def test_app(self):
+        testrun = ep.AppTest(Resource)
+        ret = testrun.get()
+        self.assertIsInstance(ret, dict)
+        self.assertIn(ep.WORKING_MSG, ret)
+
+    @skip("In the middle of making this work.")
+    def test_create_user(self):
+        """
+        See if we can successfully create a new user.
+        Post-condition: user is in DB.
+        """
+        cu = ep.CreateUser(Resource)
+        new_user = new_entity_name("user")
+        ret = cu.create(new_user)
+        users = db.get_users()
+        self.assertIn(new_user, users)
+
+    def test_create_event(self):
+        """
+        See if we can successfully create a new event.
+        Post-condition: event is in DB.
+        """
+        cr = ep.CreateEvent(Resource)
+        new_event = new_entity_name("room")
+        ret = cr.create(new_event)
+        events = db.get_events()
+        self.assertIn(new_event, events)
+
+    def event_test1(self):
+        """
+        Post-condition 1: return is a dictionary.
+        """
+        lr = ep.ListEvents(Resource)
+        ret = lr.get()
+        self.assertIsInstance(ret, dict)
+
+    def event_test2(self):
+        """
+        Post-condition 2: keys to the dict are strings
+        """
+        lr = ep.ListEvents(Resource)
+        ret = lr.get()
+        for key in ret:
+            self.assertIsInstance(key, str)
+
+    def event_test3(self):
+        """
+        Post-condition 3: the values in the dict are themselves dicts
+        """
+        lr = ep.ListEvents(Resource)
+        ret = lr.get()
+        for val in ret.values():
+            self.assertIsInstance(val, dict)
