f63d721da107d6b27b0c1278e479812e6a657bfb
Beth <90496145+Bethss@users.noreply.github.com>
2021-12-07 11:57:57 -0500

    Create mongo_test.py

---
+++ db/mongo_test.py
@@ -0,0 +1,20 @@
+import sys
+import json
+import pymongo as pm
+import bson.json_util as bsutil
+
+#from pymongo.server_api import ServerAPi
+
+# import mongo_conect as mc
+
+client = pm.MongoClient()
+print(client)
+
+#db = client ["ChatDB"]
+#print(db)
+#
+#collect_nm = sys.argv[2]
+#print(f"colect_nm=}")
+#collection = db[collect_nm]
+
+# Professor said we won't need this for the project, but to run it locally on computer to see what it does
