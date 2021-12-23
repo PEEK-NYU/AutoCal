bde3be20f3a31f24847bf86fd349b9640fa094f8
Ephil012 <Etp5501@gmail.com>
2021-12-22 19:28:55 -0500

    Added TLS/SSL for remote Mongo

--- db/db_connect.py
+++ db/db_connect.py
@@ -40,7 +40,7 @@ def get_client():
         client = pm.MongoClient(f"mongodb+srv://{user_nm}:{passwd}@"
                                 + f"{cloud_svc}/{db_nm}?"
                                 + "retryWrites=true&w=majority",
-                                server_api=ServerApi('1'))
+                                server_api=ServerApi('1'), tls=True,tlsAllowInvalidCertificates=True)
     return client
 
 
