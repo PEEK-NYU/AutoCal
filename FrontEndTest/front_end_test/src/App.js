6a447ed280c8914f4df4cefd554b3712ed431a10
Paul Liu <sl6724@nyu.edu>
2022-02-11 23:49:14 -0500

    following tutorial

--- FrontEndTest/front_end_test/src/App.js
+++ FrontEndTest/front_end_test/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'; //always required
-
-
 import {useState} from 'react'; //for setting values
-
+import {useEffect} from 'react'; //for fetching data?
+import Room from './Room'; //for React Component
 
 export default function App(){
   const [counter, setCounter] = useState(0); // A State Variable
@@ -25,21 +24,37 @@ export default function App(){
     setAString(aString + val);
   }
 
+//==========================================================
+
+
+  //invoked when element gets rendered, like init
+  //the '' should be the url for the endpoint
+  useEffect(()=>{
+    axios.get('')
+      .then((res)=>{
+        console.log(res);
+      }) //will only be executed when the above statement returns, in this case the data from the server
+  }, []) //[] is the dependency array, MUST pass it, now the funciton only gets called when mounts or rerenders
+  
+  
+
 
   return (
     <div>
 
-    {button}
-
       <button onClick={incCounter}> {/*NO (), Important, only runs when clicked*/}
         inc Counter
       </button>
-
       
       <button onClick={()=> addText(norm_val)}> {/* passing parameters */}
         add text
-      }
       </button>
+
+
+    {/*An React Component, Defined Elsewhere, see import*/}
+      <div>
+        <Room roomName={"Room A"} numUsers={3}/>
+      </div>
       
 
 
