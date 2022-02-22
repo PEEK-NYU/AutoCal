d43d76318631d2e9fdb66e4db80853cf6cda8cf9
Beth <90496145+Bethss@users.noreply.github.com>
2022-02-22 10:45:10 -0500

    Update mongo_port.sh

--- db/mongo_port.sh
+++ db/mongo_port.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 export passwd=$MONGO_PASSWD
-export db="chatDB"
+export db="peekDB"
 export collect="users"
 export key="userName"
+
+python3 mongo_port.py $db $collect $key $passwd
+
+export collect="events"
+export key="_id"
+
+python3 mongo_port.py $db $collect $key $passwd
+
+export collect="breaks"
+export key="_id"
 
 python3 mongo_port.py $db $collect $key $passwd
