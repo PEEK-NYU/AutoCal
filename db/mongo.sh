30c087b80197d82f38b80aef8c3f502d2b5b4701
Beth <90496145+Bethss@users.noreply.github.com>
2022-02-22 10:43:58 -0500

    Update mongo.sh
    Looks like Ethan made one for peek!

--- db/mongo.sh
+++ db/mongo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mongosh "mongodb+srv://serverlessinstance0.irvgp.mongodb.net/chatDB" --apiVersion 1 --username gcallah
+mongosh "mongodb+srv://serverlessinstance0.mvrqy.mongodb.net/peekDB" --apiVersion 1 --username peekUser
