7eadf7c3d3482bf31050164497ec4fac6ecd9065
Kora <korastuff@gmail.com>
2022-03-03 11:33:24 -0500

    Created Session Model
    ~how an instance of a user might interact with the browser outside of endpoints
    ~login example

---
+++ db/user_inst.py
@@ -0,0 +1,35 @@
+"""
+test file to simulate how a user's instance will interact with the system...
+"""
+import user_data as udata
+
+def login_attempt():
+    username = "example_uname"
+    password = "example_pw"
+    # TODO: attach to react frontend to fill-out this method
+    return username, password
+def main():
+    # init
+    session_in_progress = False
+    curr_user = None
+
+    #runtime
+    session_in_progress = True
+    query = ""
+    while session_in_progress:
+        # Note: print statements are temp -- should be put in frontend, not console
+        if query == "logging":
+            user_data = login_attempt()
+            ret = udata.log_in(user_data[0], user_data[1])
+            if ret == udata.NOT_FOUND:
+                print("Incorrect username and password combination... please try again")
+            else:
+                print("User", user_data[0], "successfully logged in")
+                curr_user = ret
+        elif query == "user":
+            print(curr_user)
+        elif query == "quit":
+            session_in_progress = False
+        else:  # shows current session info for testing
+            print("Session in progress... " + str(curr_user), '\r')
+
