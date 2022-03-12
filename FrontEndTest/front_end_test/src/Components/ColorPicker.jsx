f511705382648308bc2eb980e5a9ab0357f2561d
Paul Liu <sl6724@nyu.edu>
2022-03-11 21:58:34 -0500

    Color input & Date input components

---
+++ FrontEndTest/front_end_test/src/Components/ColorPicker.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function ColorPicker(){
+
+	return (
+	    <div class="sub-main">
+		    <input type="color" id="head" name="head"
+		           value="#e66465"/>
+	    </div>
+	)
+}
