bd931e7d07fa7e10de159f9a873ee984e77307b1
Paul Liu <sl6724@nyu.edu>
2022-03-25 01:39:24 -0400

    Submodule problem fixed

---
+++ FrontEnd/src/components/NavButton/NavButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+//import {useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+import './NavButton.css';
+
+
+export default function NavButton({text,subpath}){
+	const history = useHistory();
+
+	function navigateToPage(path) {
+		history.push(path);
+	}
+
+	return (
+	    <button
+	      onClick={() => navigateToPage(subpath)}
+	      className="button-nav"
+	    >
+	      {text}
+	    </button>
+	)
+
+}
---
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import NavButton from '../../components/NavButton/NavButton'
+
+export default function Home(){
+  return (
+    <div className="content">
+      <h1>AutoCal</h1>
+      <p> CALENDER PLANNER FOR ALL </p>
+      <NavButton text={'View All Rooms'} subpath={'/rooms'}/>
+      <NavButton text={'View All Users'} subpath={'/users'}/>
+      <NavButton text={'Login/Register'} subpath={'/logreg'}/>
+    </div>
+  );
+};
+
