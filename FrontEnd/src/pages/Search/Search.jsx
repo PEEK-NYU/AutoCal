814c1c6f5363c4ceff89745f67faffb62f67151a
Paul Liu <sl6724@nyu.edu>
2022-04-15 02:31:00 -0400

    Search page modified & Trying to fix css for EventItem

--- FrontEnd/src/pages/Search/Search.jsx
+++ FrontEnd/src/pages/Search/Search.jsx
@@ -1,19 +1,71 @@
+//https://www.emgoto.com/react-search-bar/
+
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useHistory} from 'react-router-dom';
 
 import EventItem from '../../components/EventItem/EventItem';
 import PageTitle from '../../components/PageTitle/PageTitle';
-import NavButton from '../../components/NavButton/NavButton'
+import NavButton from '../../components/NavButton/NavButton';
 
 import {backendurl} from '../../config';
 
 import './Search.css';
 
+const events = [
+  { eventname: 'Meeting1', start_time: '11:00', location: '1' },
+  { eventname: 'Meeting2', start_time: '12:00', location: '2' },
+  { eventname: 'Gym', start_time: '13:00', location: '3' },
+  { eventname: 'Bar', start_time: '14:00', location: '4' },
+];
+
 export default function Search() {
+
+  //function for filtering events
+  const filterEvents = (events, query) => {
+    if (!query) {
+        return events;
+    }
+    return events.filter((event) => {
+        const eventname = event.eventname.toLowerCase();
+        return eventname.includes(query);
+    });
+  };
+ 
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('input'); //the input string
+  const filteredEvents = filterEvents(events, query); //list of events matching
+
   return (
     <div className="content">
+
       <PageTitle text={'Event Search'}/>
+      
+      <form action="/search" method="get">
+        <input
+            type="text"
+            id="header-search"
+            placeholder="Enter Event Name"
+            name="input"
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <div className="event-list">
+        {filteredEvents ? filteredEvents.map((event, index) => (
+          <EventItem
+            key={`${event.evetnname}-${index}`}
+            name={event.eventname}
+            start_time={event.start_time}
+            location={event.location}
+          />
+        )) : (
+          <div className="events-empty">
+            <p> Didn't find any result </p>
+          </div>
+        )}
+      </div>
+
       <NavButton text={'Go Back Home'} subpath={'/'}/>
     </div>
   )
