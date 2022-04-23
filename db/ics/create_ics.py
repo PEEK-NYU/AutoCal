e38f8964094d2f323332aedf2b00ed5809f05897
Beth <90496145+Bethss@users.noreply.github.com>
2022-04-23 19:34:21 -0400

    Update create_ics.py
    😂😂 icalendar might not be ics--
    uhh page: https://pypi.org/project/ics/

--- db/ics/create_ics.py
+++ db/ics/create_ics.py
@@ -90,4 +90,18 @@ print()
 how_many_leap_days = calendar.leapdays(2000,3000)
 print(how_many_leap_days)
 
+
+TODO: check below, change above?
+from ics import Calendar, Event
+c = Calendar()
+e = Event()
+e.name = "My cool event"
+e.begin = '2014-01-01 00:00:00'
+c.events.add(e)
+c.events
+# [<Event 'My cool event' begin:2014-01-01 00:00:00 end:2014-01-01 00:00:01>]
+with open('my.ics', 'w') as my_file:
+    my_file.writelines(c)
+# and it's done !
+
 """
