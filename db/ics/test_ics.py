c344c6c8d0e87b6cc72aada8bfc5bbc14460be51
Bethss <ea2327@nyu.edu>
2022-04-26 12:51:18 -0400

    Adding tests for create_ics and test_calendar.ics is just the test ics file being manipulated by tests

---
+++ db/ics/test_ics.py
@@ -0,0 +1,27 @@
+"""
+WIP
+"""
+import db.ics.create_ics as ci
+
+
+organizer = input("Enter organizer name: ")
+title = input("Enter event title: ")
+desc = input("Enter event description: ")
+start = input("Enter start time (20010101 00:00:00): ")
+end = input("Enter end time (20010101 00:00:00): ")
+attendees = input("Enter Attendees ('John' or 'mike, lola, etc' or 'none'): ")
+location = input("Enter Location: ")
+
+test_event = ci.create_event(organizer, title, start, end, desc, attendees, location)
+
+test_cal = ci.add_to_cal(test_event, 'test_calendar.ics')
+
+print(test_cal)
+
+
+
+
+
+
+
+
