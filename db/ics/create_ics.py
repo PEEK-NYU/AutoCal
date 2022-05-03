afc1f7bbcb77ca7c286e3cef89e613a3cf138163
Kora <korastuff@gmail.com>
2022-05-03 13:33:20 -0400

    Fixing up cal stuff

--- db/ics/create_ics.py
+++ db/ics/create_ics.py
@@ -1,16 +1,13 @@
 """
 This file creates a a new event using users details
-
 (Later) Parses users uploaded calendar to find open stimes
-
 (Later) Displays suggests times to user
-
-Manipulate calendars, connects extsiting users to events and stores event data in database
-
-
+Manipulate calendars, connects extsiting users to events and
+    stores event data in database
 """
+
 from ics import Calendar, Event
-from datetime import datetime
+# from datetime import datetime
 from db import user_data as udata
 # from db import connect_data
 from db import event_data as edata
@@ -22,11 +19,13 @@ NOT_FOUND = 1
 DUPLICATE = 2
 
 
-def create_event(organizer, title, start, end, desc, attendees = None, location = None):
+def create_event(organizer, title, start, end, desc, attendees=None,
+                 location=None):  # noqa E501
+    """ ... """
     # TODO: Create event object with entered data....
     new_event = Event(name=title)
     new_event.organizer = organizer
-    new_event.begin = start 
+    new_event.begin = start
     new_event.end = end
     new_event.description = desc
     new_event.location = location
@@ -35,15 +34,13 @@ def create_event(organizer, title, start, end, desc, attendees = None, location 
             att_lst = list(attendees.split(", "))
             for persons in att_lst:
                 new_event.add_attendee(persons)
-        
         else:
             new_event.add_attendee(attendees)
-
     return new_event
-    
+
 
 def add_to_cal(event, file):
-    
+    """ ... """
     cal.events.add(event)
     with open(file, 'w') as my_file:
         my_file.writelines(cal)
@@ -59,9 +56,8 @@ def convert_event_dict(ics_data):
                edata.ETIME: end_time, edata.LOC: location,
                edata.DESC: description}
     """
-    event_data = {edata.ENAME: "", edata.STIME: "",
-                   edata.ETIME: "", edata.LOC: "",
-                   edata.DESC: ""}
+    # event_data = {edata.ENAME: "", edata.STIME: "", edata.ETIME: "",
+    #               edata.LOC: "", edata.DESC: ""}
     # TODO: finish
     return {}
 
