eb05ca24468617a02fc69b7355c73c91b492b68a
Kora <korastuff@gmail.com>
2022-05-03 13:13:43 -0400

    Updating Tests

--- db/tests/test_data.py
+++ db/tests/test_data.py
@@ -27,6 +27,7 @@ fake_key = "FAKE_KEY"
 
 # fake user data
 FAKE_USERNAME = "user name" + rand_name() + fake_key
+UPDATED_FAKE_USERNAME = "user name" + rand_name() + fake_key
 FAKE_PW = "password" + rand_name()
 FAKE_EM = "test@testemail.com"
 fake_u_data = {udata.UNAME: FAKE_USERNAME,
@@ -116,11 +117,38 @@ class DBTestCase(TestCase):
             self.assertEquals(test_data[key], value)
         # print("User Data Found:", test_data)
 
+    def test_find_user(self):
+        """ can we find our user via keyword? """
+        our_user = udata.find_user(fake_key)
+        self.assertIn(self.test_uid, our_user.keys())
+
     def test_login(self):
         """ can we log into a user? """
         logged = udata.log_in(FAKE_USERNAME, FAKE_PW)
         self.assertEquals(self.test_uid, logged)
         # print("User", FAKE_USERNAME, "logged in!")
+
+    def test_get_emails(self):
+        """ can we find our email in the set of all emails? """
+        ems = udata.get_emails()
+        self.assertIn(FAKE_EM, ems)
+
+    def test_email_exists(self):
+        """ can we make sure our specific email exists? """
+        self.assertEquals(udata.OK, udata.email_exists(FAKE_EM))
+
+    def test_valid_email(self):
+        """ is our email validation working? """
+        self.assertEquals(udata.OK, udata.valid_email(FAKE_EM))
+        self.assertEquals(udata.OK, udata.valid_email(FAKE_EM+rand_name()))
+        self.assertEquals(udata.NOT_FOUND, udata.valid_email("bademail"))
+
+    # def test_update_username(self):
+    #     """ can we update our user's username and login with the new version? """
+    #     ret = udata.update_username(self.test_uid, UPDATED_FAKE_USERNAME)
+    #     self.assertEquals(ret, udata.OK)
+    #     logged = udata.log_in(UPDATED_FAKE_USERNAME, FAKE_PW)
+    #     self.assertEquals(self.test_uid, logged)
 
     # EVENT TESTS
     def test_get_events(self):
@@ -157,16 +185,33 @@ class DBTestCase(TestCase):
         temp_events = edata.find_event(self.test_uid, fake_key)
         self.assertIn(self.test_eid, temp_events.keys())
 
+    # CONNECT TESTS
     def test_connection(self):
         """
         test that the connection made between our test event and user exists
         """
-        self.assert_(cdata.is_connected(self.test_eid, self.test_uid))
+        self.assertTrue(cdata.is_connected(self.test_eid, self.test_uid))
 
+    def test_get_connection(self):
+        """ test that we can find our connection in all connections """
+        connects = cdata.get_all_connections()
+        fake_cdata = {cdata.CUSER: self.test_uid, cdata.CEVENT: self.test_eid}
+        self.assertIn(fake_cdata, connects.values())
+
+    def test_connection_eid(self):
+        """ test that we can access our connection via eid """
+        self.assertEquals(self.test_uid, cdata.get_connection_from_eid(self.test_eid))
+
+    # DELETION TESTS
     def test_del_event(self):
-        """ see if we can delete our test event """
+        """
+        see if we can delete our test event
+        and its corresponding connection
+        """
         deleted = edata.del_event(self.test_eid, self.test_uid)
         self.assertNotIn(self.test_eid, edata.get_all_events().keys())
+        # de
+        self.assertFalse(cdata.is_connected(self.test_eid, self.test_uid))
 
     def test_del_user(self):
         """ see if we can delete our test user """
