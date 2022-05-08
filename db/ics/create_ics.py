51a152838c3cd964a07c8f63803a1325cde08ba2
Beth <90496145+Bethss@users.noreply.github.com>
2022-05-08 00:37:43 -0400

    Update create_ics.py
    variable mispelled

--- db/ics/create_ics.py
+++ db/ics/create_ics.py
@@ -6,49 +6,46 @@ Manipulate calendars, connects extsiting users to events and
     stores event data in database
 """
 
-#from ics import Calendar, Event
+from ics import Calendar, Event
 #from datetime import datetime
 from db import user_data as udata
 # from db import connect_data
 from db import event_data as edata
 import jicson # Python ICS to JSon library @ https://github.com/CalyFactory/python-jicson
 
-#cal = Calendar()  # demo empty calendar
-
 OK = 0
 NOT_FOUND = 1
 DUPLICATE = 2
 
+def create_event(organizer, title, start, end, desc, attendees=None,
+                 location=None):  # noqa E501
+    """ 
+    Create event object with user's event details 
+    """
+    new_event = Event(name=title)
+    new_event.organizer = organizer
+    new_event.begin = start
+    new_event.end = end
+    new_event.description = desc
+    new_event.location = location
+    if attendees is not None:
+        if ',' in attendees:
+            att_lst = list(attendees.split(", "))
+            for persons in att_lst:
+                new_event.add_attendee(persons)
+        else:
+            new_event.add_attendee(attendees)
+    return new_event
 
-#def create_event(organizer, title, start, end, desc, attendees=None,
-#                 location=None):  # noqa E501
-#    """ 
-#    Create event object with user's event details 
-#    """
-#    new_event = Event(name=title)
-#    new_event.organizer = organizer
-#    new_event.begin = start
-#    new_event.end = end
-#    new_event.description = desc
-#    new_event.location = location
-#    if attendees is not None:
-#        if ',' in attendees:
-#            att_lst = list(attendees.split(", "))
-#            for persons in att_lst:
-#                new_event.add_attendee(persons)
-#        else:
-#            new_event.add_attendee(attendees)
-#    return new_event
-#
-#
-#def add_to_cal(event, user_ics_data):
-#    """ 
-#    Add event to user ics file 
-#    """
-#    cal.events.add(event)
-#    with open(user_ics_data, 'w') as my_file:
-#        my_file.writelines(cal)
-#    return cal
+def add_to_cal(event, user_ics_data):
+    """ 
+    Add event to user ics file 
+    """
+    cal = Calendar()  #empty calendar
+    cal.events.add(event)
+    with open(user_ics_data, 'w') as my_file:
+        my_file.writelines(cal)
+    return cal
 
 def convert_event_dict(ics_data):
     """
@@ -62,7 +59,7 @@ def convert_event_dict(ics_data):
     result = jicson.fromFile(ics_data)
     cal = result['VCALENDAR']
     events = cal[0]['VEVENT']
-    event_date = {}
+    event_data = {}
     if len(events)>0:
         for event in events:
             event_data = {edata.ENAME: event['SUMMARY'], edata.STIME: event['DTSTART'], edata.ETIME: event['DTEND'],
